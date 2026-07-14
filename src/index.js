aea7db6a8b4fa53c87f7d8d0dbc0734e44faec34
Akshansh Ranjan <akshanshranjan0519@gmail.com>
2026-07-15 00:12:09 +0530

    feat: add cors middleware to booking service to allow client browser requests

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const apiRoutes = require('./routes');
 const db = require('./models');
 
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
